50557805cd76ee52f7c1ab92eee3a0b24ad64b53
foxnorth228 <khitrii03@gmail.com>
2022-04-01 12:52:52 +0300

    Add Fibonacci position number

---
+++ educ1.py
@@ -0,0 +1,23 @@
+FI = (1 + 5**(0.5)) / 2
+
+def call_the_function():
+    value = 0
+    is_not_right_answer = True
+    while(is_not_right_answer):
+        value = input("Enter position of the fibonacci sequence number\n")
+        try:
+            value = int(value)
+            if(value <= 0):
+                raise ValueError()
+        except ValueError:
+            print("Please, input natural number")
+        else:
+            is_not_right_answer = False
+    print(f"Required number is {fibonacci_bine(value)}")
+
+
+def fibonacci_bine(n=0):
+    num = ((FI**n) - (1 / ((-FI)**n))) / (2*FI - 1)
+    return round(num)
+
+call_the_function()
