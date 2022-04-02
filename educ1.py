27aac2235f1fc6aa30a972e215692b8f3570e98e
foxnorth228 <khitrii03@gmail.com>
2022-04-02 17:53:11 +0300

    Fix EOF, overflow and other errors

--- educ1.py
+++ educ1.py
@@ -1,19 +1,28 @@
 FI = (1 + 5**(0.5)) / 2
 
-def call_the_function():
-    value = 0
-    is_not_right_answer = True
-    while(is_not_right_answer):
-        value = input("Enter position of the fibonacci sequence number\n")
-        try:
-            value = int(value)
-            if(value <= 0):
-                raise ValueError()
-        except ValueError:
-            print("Please, input natural number")
-        else:
-            is_not_right_answer = False
-    print(f"Required number is {fibonacci_bine(value)}")
+def show_fibonacci_number():
+    try:
+        value = 0
+        is_not_right_answer = True
+        while(is_not_right_answer):
+            try:
+                value = input("Enter position of the fibonacci sequence number\n")
+            except EOFError:
+                print("Error with input. Maybe, you didn't connect program to console")
+            try:
+                value = int(value)
+                if(value <= 0):
+                    raise ValueError()
+            except ValueError:
+                print("Please, input natural number")
+            else:
+                is_not_right_answer = False
+        print(f"Required number is {fibonacci_bine(value)}")
+    except OverflowError as e:
+        print("You input very big position. We can't calculate this number.")
+    except BaseException as e:
+        print(f"Something is wrong. {e}")
+        
 
 
 def fibonacci_bine(n=0):
@@ -21,7 +30,7 @@ def fibonacci_bine(n=0):
     return round(num)
 
 def main():
-    call_the_function()
+    show_fibonacci_number()
 
 if __name__ == "__main__":
     main()
