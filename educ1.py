fa14e8eaa7d118aa66f7064b06967852baf73b97
foxnorth228 <khitrii03@gmail.com>
2022-04-30 10:58:07 +0300

    Fix bug with no interface

--- educ1.py
+++ educ1.py
@@ -1,5 +1,3 @@
-FI = (1 + 5**(0.5)) / 2
-
 def show_fibonacci_number():
     try:
         value = 0
@@ -9,6 +7,7 @@ def show_fibonacci_number():
                 value = input("Enter position of the fibonacci sequence number\n")
             except EOFError:
                 print("Error with input. Maybe, you didn't connect program to console")
+                return
             try:
                 value = int(value)
                 if(value <= 0):
@@ -22,10 +21,9 @@ def show_fibonacci_number():
         print("You input very big position. We can't calculate this number.")
     except BaseException as e:
         print(f"Something is wrong. {e}")
-        
-
-
+      
 def fibonacci_bine(n=0):
+    FI = (1 + 5**(0.5)) / 2
     num = ((FI**n) - (1 / ((-FI)**n))) / (2*FI - 1)
     return round(num)
 
